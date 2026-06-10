9fe06dda7d0df7965c969ad8b2a9faf421297400
Marcos Ramirez <f.marcos.rd@gmail.com>
2026-06-10 15:47:12 -0600

    primer commit

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+import { src, dest, watch, series } from 'gulp' 
+import * as dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+
+const sass = gulpSass(dartSass)
+
+export function css( done ) {
+		src('src/scss/app.scss', {sourcemaps: true})
+			.pipe ( sass().on('error', sass.logError) )
+			.pipe( dest('dist/css', {sourcemaps: '.'}) )
+		done()
+}
+
+export function dev() {
+	watch('src/scss/**/*.scss', css)
+}
+
+export default series(css, dev);
